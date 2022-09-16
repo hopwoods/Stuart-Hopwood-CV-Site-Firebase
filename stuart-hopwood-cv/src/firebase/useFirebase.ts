0395ba1bf8a964060c077f7e3ad9b1054c5480f4
Stuart Hopwood <stoo.hopwoo@gmail.com>
2022-09-16 14:27:10 +0100

    Add Google App Check

--- stuart-hopwood-cv/src/firebase/useFirebase.ts
+++ stuart-hopwood-cv/src/firebase/useFirebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
-
-// TODO: Add SDKs for Firebase products that you want to use
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 export function useFirebase() {
 
@@ -19,9 +18,15 @@ export function useFirebase() {
     const firebaseApp = initializeApp(firebaseConfig);
     //const analytics = getAnalytics(firebaseApp);
 
+    // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+    // key is the counterpart to the secret key you set in the Firebase console.
+    const appCheck = initializeAppCheck(firebaseApp, {
+        provider: new ReCaptchaV3Provider('6LfcdQIiAAAAAGFXyfauaf1CxTmjX014fJzJnQWI'),
+        isTokenAutoRefreshEnabled: true
+    });
+
 
     return { firebaseApp }
-
 }
 
 
