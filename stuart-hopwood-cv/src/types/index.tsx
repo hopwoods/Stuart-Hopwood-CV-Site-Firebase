fca0daba254adb8522217ed595f812c2cbeea829
Stuart Hopwood <stoo.hopwoo@gmail.com>
2022-10-10 11:56:41 +0100

    Fix build issue

--- stuart-hopwood-cv/src/types/index.tsx
+++ stuart-hopwood-cv/src/types/index.tsx
@@ -1,6 +1,6 @@
 export type { AboutText } from './aboutText'
 export type { ApiAccessTokenState } from './ApiAccessTokenState'
-export type { AddSkillButtonProps, ButtonColors, ButtonSizes, EditSkillButtonProps, SkillButtonProps } from './buttonTypes'
+export type { SkillButtonProps } from './buttonTypes'
 export type { SkillExampleProps, SkillProps } from './skillProps'
 export type { SocialButtonLinkProps } from './socialButtonLinkProps'
 
